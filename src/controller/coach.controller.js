f2090b1b98cf30a94a8c23ed16cf97f77e30115e
welfar <83843290+welfar@users.noreply.github.com>
2021-06-24 18:56:32 -0500

    Se agrega las validaciones del coach, las rutas signup y signin y el … (#9)
    * Se agrega las validaciones del coach, las rutas signup y signin y el async await en el coach.controller
    
    * Se elimina el metodo create de coach.controller
    
    * correcion del metodo pre-save

--- src/controller/coach.controller.js
+++ src/controller/coach.controller.js
@@ -1,62 +1,91 @@
 const Coach = require("../models/coach.model");
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 module.exports = {
-	create(req, res) {
-		const { body } = req;
+  async signup(req, res) {
+    try {
+      const { body } = req;
+      const coach = await Coach.create(body);
+  
+      const token = jwt.sign({ coachId: coach._id }, process.env.SECRET, {
+        expiresIn: 60 * 60 * 24 * 365,
+      });
+  
+      res.status(201).json({ token });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+  
+	async list(req, res) {
+    try {
+      const coaches = await Coach.find()
+      res.status(200).json(coaches)
+    } catch (err) {
+      res.status(400).json({ message: err.message }); 
+    }
+	},
 
-		Coach.create(body)
-			.then((coach) => {
-				res.status(201).json(coach);
-			})
-			.catch((err) => {
-				res.status(400).json({ message: err.message });
-			});
+	async show(req, res) {
+    try{
+      const { coachId } = req.params
+      const coach = await Coach.findById(coachId)
+      res.status(200).json(coach);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
 	},
-	list(req, res) {
-		Coach.find()
-			.then((coaches) => {
-				res.status(200).json(coaches);
-			})
-			.catch((err) => {
-				res.status(400).json({ message: err.message });
-			});
-	},
-	show(req, res) {
-		const { coachId } = req.params;
 
-		Coach.findById(coachId)
-			.then((coach) => {
-				res.status(200).json(coach);
-			})
-			.catch((err) => {
-				res.status(404).json({ message: err.message });
-			});
+	async update(req, res) {
+    try {
+      const {
+        params: { coachId },
+        body,
+      } = req;
+      const coach = await Coach.findByIdAndUpdate(coachId, body, {
+        new: true,
+      })
+      res.status(200).json(coach);
+    } catch(err) {
+				res.status(400).json({ message: err.message })
+			}
 	},
-	update(req, res) {
-		const {
-			params: { coachId },
-			body,
-		} = req;
 
-		Coach.findByIdAndUpdate(coachId, body, {
-			new: true,
-		})
-			.then((coach) => {
-				res.status(200).json(coach);
-			})
-			.catch((err) => {
-				res.status(400).json({ message: err.message });
-			});
+	async destroy(req, res) {
+    try {
+      const { coachId } = req.params;
+      const coach = await Coach.findByIdAndDelete(coachId)
+      res.status(200).json(coach);
+    } catch(err) {
+				res.status(400).json({ message: err.message })
+			}
 	},
-	destroy(req, res) {
-		const { coachId } = req.params;
 
-		Coach.findByIdAndDelete(coachId)
-			.then((coach) => {
-				res.status(200).json(coach);
-			})
-			.catch((err) => {
-				res.status(400).json({ message: err.message });
-			});
-	},
-};
+
+  async signin(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      const coach = await Coach.findOne({ email });
+
+      if (!coach) {
+        throw new Error("Contraseña o correo invalido");
+      }
+
+      const isValid = await bcrypt.compare(password, coach.password);
+
+      if (!isValid) {
+        throw new Error("Contraseña o correo invalido");
+      }
+
+      const token = jwt.sign({ coachId: coach._id }, process.env.SECRET, {
+        expiresIn: 60 * 60 * 24 * 365,
+      });
+
+      res.status(201).json({ token });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+}
--- src/routes/coach.js
+++ src/routes/coach.js
@@ -6,5 +6,7 @@ router.route("/").get(coachController.list);
 router.route("/:coachId").get(coachController.show);
 router.route("/:coachId").put(coachController.update);
 router.route("/:coachId").delete(coachController.destroy);
+router.route("/signup").post(coachController.signup);
+router.route("/signin").post(coachController.signin);
 
 module.exports = router;
