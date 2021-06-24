ca8ce8f5cb6fe0fe08dc8dc50760170b312584fb
Natalia <nbedoyaruiz@gmail.com>
2021-06-24 06:30:10 -0500

    Se revierte el cambio en bd para continuar trabajando con then/catch

--- src/db.js
+++ src/db.js
@@ -1,24 +1,13 @@
 const mongoose = require("mongoose");
 
 function connect() {
-  const mongoURI = "mongodb://localhost:27017/crossfitapp";
-  const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
-
-  mongoose.connect(mongoURI, options);
-
-  const { connection } = mongoose;
-
-  connection.once("open", () => {
-    console.log("Connection established successfully");
-  });
-  connection.on("error", (err) => {
-    console.log("Something went wrong", err);
-  });
-
-  return connection;
+  mongoose
+    .connect("mongodb://localhost:27017/crossfitapp", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connection established successfully"))
+    .catch((err) => console.log("Something went wrong", err));
 }
 
 module.exports = connect;
