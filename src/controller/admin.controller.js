fe7fb5ae9ac83f1dbed18c98bb8aca5da3a1a820
Natalia <nbedoyaruiz@gmail.com>
2021-06-30 16:23:10 -0500

    Se hacen las validaciones para administrador, con los métodos list, show, update, destroy, signup, sigin

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -1,31 +1,25 @@
+const jwt = require("jsonwebtoken")
+const bcrypt=require('bcrypt')
+
 const Admin = require("../models/admin.model");
 
 module.exports = {
-  async create(req, res) {
-    try {
-      const { body } = req;
-
-      const admin = await Admin.create(body);
-      res.status(201).json(admin);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-  },
   async list(req, res) {
     try {
       const admins = await Admin.find();
       res.status(200).json(admins);
     } catch (err) {
-      res.status(400).json({ message: err.message });
+      res.status(400).json({ message: "Error en la obtención de los datos." });
     }
   },
+
   async show(req, res) {
     try {
       const { adminId } = req.params;
       const admin = await Admin.findById(adminId);
       res.status(200).json(admin);
     } catch (err) {
-      res.status(404).json({ message: err.message });
+      res.status(404).json({ message: "Error en la obtención de los datos" });
     }
   },
 
@@ -38,7 +32,7 @@ module.exports = {
       const admin = await Admin.findByIdAndUpdate(adminId, body, { new: true });
       res.status(200).json(admin);
     } catch (err) {
-      res.status(400).json({ message: err.message });
+      res.status(400).json({ message: "Error actuallizando los datos" });
     }
   },
 
@@ -48,7 +42,47 @@ module.exports = {
       const admin = await Admin.findByIdAndDelete(adminId);
       res.status(200).json(admin);
     } catch (err) {
-      res.status(400).json({ message: err.message });
+      res.status(400).json({ message: "Error eliminando los datos" });
     }
   },
+
+  async signup(req,res){
+    try{
+      const{body} = req;
+      const admin = await Admin.create(body);
+      const token = jwt.sign({adminId: admin._id}, process.env.SECRET, {
+        expiresIn:60*60*24*365,
+      });
+      res.status(201).json({token})
+        }catch(error){
+      res.status(400).json('Error registrando un administrador')
+    }
+  },
+
+  async signin(req, res) {
+    try {
+      const { email, password } = req.body
+      const admin = await Admin.findOne({ email })
+      if (!admin) {
+        throw new Error('correo inválido')
+      }
+      const isValid = await bcrypt.compare(password, admin.password)
+      console.log(password)
+      console.log(admin.password)
+      console.log(isValid)
+      if (!isValid) {
+        throw new Error('contraseña inválido')
+      }
+      const token = jwt.sign({ adminId: admin._id }, process.env.SECRET, {
+        expiresIn: 60 * 60 * 24 * 365
+      })
+      res.status(201).json({ token })
+    } catch (error) {
+      res.status(400).json({message: error.message})
+      console.log(error)
+    }
+  }
+
+
+
 };
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const adminController = require("../controller/admin.controller");
 
-router.route("/").post(adminController.create);
 router.route("/").get(adminController.list);
 router.route("/:adminId").get(adminController.show);
 router.route("/:adminId").put(adminController.update);
 router.route("/:adminId").delete(adminController.destroy);
+router.route('/signup').post(adminController.signup)
+router.route('/signin').post(adminController.signin)
 
 module.exports = router;
