bbf67f37b3718b880b90fddd8c0a9771e867ec79
Natalia <nbedoyaruiz@gmail.com>
2021-07-10 15:09:09 -0500

    Se eliminan archivos que se deben configurar distinto

--- src/routes/wod.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const wodController = require("../controller/wod.controller");
-
-router.route("/").post(wodController.create);
-
-module.exports = router;
--- src/server.js
+++ src/server.js
@@ -3,13 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const connect = require("./db");
+
 const userRouter = require("./routes/user");
 const coachRouter = require("./routes/coach");
 const adminRouter = require("./routes/admin");
-const wodRouter = require("./routes/wod");
-const { auth } = require("./utils/middlewares");
-const newsRouter = require("./routes/news");
 
+const { auth } = require("./utils/middlewares");
 
 const port = process.env.PORT || 8000;
 const app = express();
@@ -22,13 +21,11 @@ app.use(morgan("dev"));
 app.use("/user", userRouter);
 app.use("/coaches", coachRouter);
 app.use("/admin", adminRouter);
-app.use("/wod", wodRouter);
-app.use("/news", newsRouter);
 
 app.get("/", auth, (req, res) => {
   res.status(200).json({ message: "estás autenticado" });
 });
 
 app.listen(port, () => {
-	console.log(`App running at http://localhost:${port}`);
+  console.log(`App running at http://localhost:${port}`);
 });
