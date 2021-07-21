f6e4a43af90de24b75b66fe4ce2c659a79477989
welfar <83843290+welfar@users.noreply.github.com>
2021-07-21 10:32:08 -0500

    Se configura MVC para actualizacion de datos y foto del coach (#27)

--- src/routes/coach.js
+++ src/routes/coach.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const coachController = require("../controller/coach.controller");
-const { auth, formData } = require("../utils/middlewares");
+const { auth, formData, coachFilter } = require("../utils/middlewares");
 
 router.route("/").get(coachController.list);
 router.route("/coachInfo").get(auth, coachController.show);
-router.route("/coachProfilePic").put(auth, formData, coachController.update);
-router.route("/:coachId").put(coachController.update);
+router.route("/coachProfilePic").put(auth, coachFilter, formData, coachController.update);
+router.route("/coachUpdate").put(auth, coachController.update);
 router.route("/:coachId").delete(coachController.destroy);
 router.route("/signup").post(coachController.signup);
 router.route("/signin").post(coachController.signin);
